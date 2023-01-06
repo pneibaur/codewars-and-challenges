b70b26adb3a8ade944db2c25bbaae288213d6fca
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2023-01-06 15:37:46 -0700

    adds panagram challenge

--- 7-kyu/main.py
+++ 7-kyu/main.py
@@ -82,3 +82,29 @@ def two_sum(numbers, target):
             diff_idx = numbers.index(diff, idx + 1)
             return [idx, diff_idx]
     return []
+
+# --------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+
+"""
+A pangram is a sentence that contains every single letter 
+of the alphabet at least once. For example, 
+the sentence "The quick brown fox jumps over the lazy dog" 
+is a pangram, because it uses the letters A-Z at least once 
+(case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. 
+Return True if it is, False if not. 
+Ignore numbers and punctuation.
+"""
+
+def is_pangram(s):
+    az_test = []
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for char in s: 
+        if char.lower() in alphabet and char not in az_test: 
+            az_test.append(char.lower())
+    if len(az_test) == len(alphabet):
+        return True
+    else: 
+        return False
