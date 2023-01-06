9baf1423f37502330bde0999a23e4fb008e09257
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2023-01-06 15:23:51 -0700

    two_sum challenge

--- 7-kyu/main.py
+++ 7-kyu/main.py
@@ -51,5 +51,34 @@ def first_non_repeating_letter(string):
 # --------------------------------------------------------------------------------------------
 
 """
+Write a function that takes an array 
+of numbers (integers for the tests) 
+and a target number. It should find two different 
+items in the array that, when added together, 
+give the target value. The indices of these items 
+should then be returned in a tuple / list 
+(depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, 
+some tests may have multiple answers; 
+any valid solutions will be accepted.
+
+The input will always be valid 
+(numbers will be an array of length 2 or greater, 
+and all of the items will be numbers; target will 
+always be the sum of two different items from that array).
+
+two_sum([1, 2, 3], 4) # returns [0, 2] or [2, 0]
 
 """
+
+def two_sum(numbers, target):
+    for idx, num in enumerate(numbers):
+        slice1 = numbers[:idx]
+        slice2 = numbers[(idx + 1):]
+        new_list = slice1 + slice2
+        diff = target - num
+        if diff in new_list:
+            diff_idx = numbers.index(diff, idx + 1)
+            return [idx, diff_idx]
+    return []
