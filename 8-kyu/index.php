3eff89c515c3fc3789fdb7c628cf862fe23b552b
Phil Neibaur <pneibaur+github@gmail.com>
2023-01-16 14:50:46 -0700

    string pairs challenge

--- 8-kyu/index.php
+++ 8-kyu/index.php
@@ -18,13 +18,48 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0. -->
 
 function positive_sum($arr) {
-  $sum = 0;
-  if (sizeof($arr) > 0){
-    foreach ($arr as $i){
-      if ($i > 0){
-        $sum += $i;
-      };
+    $sum = 0;
+    if (sizeof($arr) > 0){
+        foreach ($arr as $i){
+            if ($i > 0){
+                $sum += $i;
+            };
+        };
     };
-  };
-  return $sum;
+    return $sum;
 }
+
+
+<!-- ------------------------------------------------------- -->
+<!-- ------------------------------------------------------- -->
+
+<!-- 
+    Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace the 
+missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef'] 
+-->
+
+function solution($str) {
+    $counter = 0;
+    $pairs = array();
+    if (strlen($str) % 2 !== 0){
+        $str = $str . "_";
+        echo "new str: $str";
+    }
+    for ($i = 0; $i < strlen($str); $i++){
+        if ($counter % 2 === 0){
+            array_push($pairs, "{$str[$i]}{$str[($i+1)]}");
+        };
+        $counter += 1;
+    };
+    return $pairs;
+}
+
+
+<!-- ------------------------------------------------------- -->
+<!-- ------------------------------------------------------- -->
